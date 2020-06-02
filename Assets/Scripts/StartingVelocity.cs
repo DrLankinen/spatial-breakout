45997d742298fe7ccfe63c0cc6ce4b1da5f12116
Elias Lankinen <elias@lankinen.xyz>
2020-06-02 09:15:28 +0300

    Initial velocity for the ball

---
+++ Assets/Scripts/StartingVelocity.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartingVelocity : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = new Vector3(0.0f, 0.0f, -2.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        // rb.velocity = new Vector3(0.0f, 0.0f, -2.0f);
+    }
+}
