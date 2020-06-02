395c1ffb4aa952d1ef7e7278959ecde691b0bd8c
Elias Lankinen <elias@lankinen.xyz>
2020-06-02 19:12:42 +0300

    Create a brick dynamically with random streght

---
+++ Assets/Scripts/CreateBricks.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateBricks : MonoBehaviour
+{
+    public GameObject brick;
+
+    void Start()
+    {
+        var newBrick = Instantiate(brick, new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.identity);
+        newBrick.transform.parent = gameObject.transform;
+        newBrick.GetComponent<Brick>().numberOfHitsLeft = Random.Range(1, 6);
+    }
+}
