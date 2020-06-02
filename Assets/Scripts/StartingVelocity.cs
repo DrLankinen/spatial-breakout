6125333403908891963037055ebe1b3eebcc1d61
Elias Lankinen <elias@lankinen.xyz>
2020-06-02 17:12:57 +0300

    Paddle
    I removed the grab feature as it's not probably needed.

--- Assets/Scripts/StartingVelocity.cs
+++ Assets/Scripts/StartingVelocity.cs
@@ -11,7 +11,8 @@ public class StartingVelocity : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        rb.velocity = new Vector3(0.0f, -2.0f, -4.0f);
+        // rb.velocity = new Vector3(0.0f, -2.0f, -4.0f);
+        rb.velocity = new Vector3(0.0f, 0.0f, 0.0f);
     }
 
     // Update is called once per frame
