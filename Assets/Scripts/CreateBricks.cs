6367066992319a077c3a279b759d6f0c872084b9
Elias Lankinen <elias@lankinen.xyz>
2020-06-03 09:18:34 +0300

    create automatically multiple bricks

--- Assets/Scripts/CreateBricks.cs
+++ Assets/Scripts/CreateBricks.cs
@@ -8,8 +8,19 @@ public class CreateBricks : MonoBehaviour
 
     void Start()
     {
-        var newBrick = Instantiate(brick, new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.identity);
-        newBrick.transform.parent = gameObject.transform;
-        newBrick.GetComponent<Brick>().numberOfHitsLeft = Random.Range(1, 6);
+        for(int r = 0; r < 3; r++)
+        {
+            for(int c = 0; c < 4; c++)
+            {
+                float width = brick.transform.localScale.x;
+                float height = brick.transform.localScale.y;
+                var newBrick = Instantiate(brick, 
+                    new Vector3(transform.position.x + width * c, 
+                                transform.position.y + height * r,
+                                transform.position.z), Quaternion.identity);
+                newBrick.transform.parent = gameObject.transform;
+                newBrick.GetComponent<Brick>().numberOfHitsLeft = Random.Range(1, 6);
+            }
+        }
     }
 }
