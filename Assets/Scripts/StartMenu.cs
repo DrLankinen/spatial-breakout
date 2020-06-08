7abea9afc62ffdc7b382f7a62111e2bbd61a48cf
Elias Lankinen <elias@lankinen.xyz>
2020-06-08 17:53:28 +0300

    Start menu partly

---
+++ Assets/Scripts/StartMenu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartMenu : MonoBehaviour
+{
+    void Awake()
+    {
+        Time.timeScale = 0;
+    }
+
+    public void StartButtonClick()
+    {
+        Time.timeScale = 1;
+    }
+}
